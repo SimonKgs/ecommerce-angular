db811707d104b5f1bb075810501b0e29a816888f
kgs <simonudemy09@gmail.com>
2024-11-27 11:10:06 +0100

    initial commit

---
+++ src/app/products/features/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../data-access/products.service';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styles: '',
+  providers: [ProductService]
+})
+export default class ProductListComponent {
+
+  private productsService = inject(ProductService);
+
+  constructor(){
+    this.productsService.getProducts().subscribe((products) => {
+      console.log(products);
+    })
+  }
+
+}
