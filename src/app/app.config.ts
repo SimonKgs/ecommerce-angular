954216cca60f78089bb7bbf2636322b22614f60e
kgs <simonudemy09@gmail.com>
2024-11-30 15:12:35 +0100

    hash location strategy added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,16 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     // withComponentInputBinding let me catch the params as inputs
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient()
+    provideHttpClient(),
+    // to avoid problems with the url on production
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+
   ]
 };
